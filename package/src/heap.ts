7bb34db830ba0ba7eb384a8606ae0601217c4d0f
Pedro Bini <dev.pedrobini@gmail.com>
2020-02-20 08:22:33 -0300

    Inject script via innerHTML

--- package/src/heap.ts
+++ package/src/heap.ts
@@ -4,10 +4,8 @@ const DEFAULT_SCRIPT_ID = 'reactjs-heap';
  * Provides the Heap script to inject.
  */
 const getHeapScript = (id: string | number) => `
-  <script type="text/javascript">
-    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-    heap.load("${id}");
-  </script>
+  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("${id}");
 `;
 
 /**
@@ -30,7 +28,7 @@ const initialize = (id: string | number) => {
 
   const script = document.createElement('script');
 
-  script.src = getHeapScript(id);
+  script.innerHTML = getHeapScript(id);
   script.id = DEFAULT_SCRIPT_ID;
   script.async = true;
 
